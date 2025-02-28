b52436758229001d4fb27b22594e0c2b647a469a
Gökhan Karaömeroğlu <itogreniyorum@gmail.com>
2025-02-28 03:32:21 +0300

    css check

--- src/app/avnibot/page.js
+++ src/app/avnibot/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[200px_12fr_10px] items-center justify-items-center min-h-screen p-8 pb-10 gap-16 sm:p-40 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-12 row-start-1 items-center m:items-start">
+    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/avni.jpg"
